d05c4ebb1741fb726b0792a7350e1d49d4b2f520
DHRUVGOLAKIYA <dhruvgolakiya07@gmail.com>
2026-07-20 10:53:07 +0530

    Update project

---
+++ login pages/index.js
@@ -0,0 +1,101 @@
+// SHOW PASSWORD
+
+
+let password = document.getElementById("password");
+
+let button = document.getElementById("showPassword");
+
+
+button.onclick = function(){
+
+
+if(password.type==="password"){
+
+    password.type="text";
+    button.innerText="Hide";
+
+}
+
+else{
+
+    password.type="password";
+    button.innerText="Show";
+
+}
+
+}
+
+
+
+// CAPS LOCK WARNING
+
+
+password.addEventListener("keyup",function(e){
+
+let warning=document.getElementById("capsWarning");
+
+
+if(e.getModifierState("CapsLock")){
+
+warning.style.display="block";
+
+}
+
+else{
+
+warning.style.display="none";
+
+}
+
+});
+
+
+
+
+// LOGIN BUTTON
+
+
+let loginButton=document.getElementById("loginBtn");
+
+
+loginButton.onclick=function(){
+
+
+let text=document.getElementById("loginText");
+
+let loading=document.getElementById("loading");
+
+
+text.classList.add("hidden");
+
+loading.classList.remove("hidden");
+
+
+setTimeout(function(){
+
+    text.classList.remove("hidden");
+    loading.classList.add("hidden");
+
+    alert("Login Successfully!");
+
+},2000);
+
+
+};
+
+
+
+
+// ENTER BUTTON SUPPORT
+
+
+document.addEventListener("keydown",function(event){
+
+if(event.key==="Enter"){
+
+    loginButton.click();
+
+}
+
+
+});
