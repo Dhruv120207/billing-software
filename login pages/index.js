77763da9757ae79087866aec413f3a2a8d7f9f86
DHRUVGOLAKIYA <dhruvgolakiya07@gmail.com>
2026-07-20 20:49:46 +0530

    Update project

--- login pages/index.js
+++ login pages/index.js
@@ -1,122 +1,97 @@
 // SHOW PASSWORD
 
-
 let password = document.getElementById("password");
-
 let button = document.getElementById("showPassword");
 
+button.onclick = function () {
 
-button.onclick = function(){
+    if (password.type === "password") {
 
+        password.type = "text";
+        button.innerText = "Hide";
 
-if(password.type==="password"){
+    } else {
 
-    password.type="text";
-    button.innerText="Hide";
+        password.type = "password";
+        button.innerText = "Show";
 
-}
+    }
 
-else{
-
-    password.type="password";
-    button.innerText="Show";
-
-}
-
-}
-
+};
 
 
 // CAPS LOCK WARNING
 
+password.addEventListener("keyup", function (e) {
 
-password.addEventListener("keyup",function(e){
+    let warning = document.getElementById("capsWarning");
 
-let warning=document.getElementById("capsWarning");
+    if (e.getModifierState("CapsLock")) {
 
+        warning.style.display = "block";
 
-if(e.getModifierState("CapsLock")){
+    } else {
 
-warning.style.display="block";
+        warning.style.display = "none";
 
-}
-
-else{
-
-warning.style.display="none";
-
-}
+    }
 
 });
 
 
 
-
-// LOGIN BUTTON
-
-
-let loginButton=document.getElementById("loginBtn");
-
-
-loginButton.onclick=function(){
-
-
-let text=document.getElementById("loginText");
-
-let loading=document.getElementById("loading");
-
-
-text.classList.add("hidden");
-
-loading.classList.remove("hidden");
-
-
-setTimeout(function(){
-
-    text.classList.remove("hidden");
-    loading.classList.add("hidden");
-
-    alert("Login Successfully!");
-
-},2000);
-
-
-};
-
-
-
-
-// ENTER BUTTON SUPPORT
-
-
-document.addEventListener("keydown",function(event){
-
-if(event.key==="Enter"){
-
-    loginButton.click();
-
-}
-
-//function login 
+// LOGIN FUNCTION
 
 function login() {
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value.trim();
+    let passwordValue = document.getElementById("password").value;
 
+    if (username === "admin" && passwordValue === "123456") {
 
-    if(username === "admin" && password === "123456"){
+        let text = document.getElementById("loginText");
+        let loading = document.getElementById("loading");
 
-        window.location.href = "../dashboard.html";
+        text.classList.add("hidden");
+        loading.classList.remove("hidden");
+
+        setTimeout(function () {
+
+            window.location.href = "dashboard.html";
+
+        }, 2000);
 
     }
 
-    else{
+    else {
 
         alert("Invalid Username or Password.");
 
     }
 
 }
+
+
+
+// LOGIN BUTTON
+
+let loginButton = document.getElementById("loginBtn");
+
+loginButton.onclick = function () {
+
+    login();
+
+};
+
+
+// ENTER BUTTON SUPPORT
+
+document.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+
+        login();
+
+    }
 
 });
